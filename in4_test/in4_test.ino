5308c13ebee4534f169ecdad52c7da241bc1319c
Vlad <putyn@ymail.com>
2017-04-30 13:09:42 +0300

    second try not yet complete

--- in4_test/in4_test.ino
+++ in4_test/in4_test.ino
@@ -1,160 +1,77 @@
+#include <ESP8266Ping.h>
 #include <Ticker.h>
-#include <SPI.h>
+#include <webui.h>
+extern settings_t settings;
 
-void update_displays(uint8_t *data);
+Ticker animation;
 
-#define LATCH_pin 16
-#define OE_pin 2
-#define HVEN_pin 15
+uint32_t unix_time = 0;
+uint32_t unix_time_ms = 0;
 
-struct dtime_t {
-  uint8_t seconds;
-  uint8_t minutes;
-  uint8_t hours;
-} local_time, old_time;
-
-struct settings_t {
-  boolean update_display;
-} settings;
-
-Ticker ticks;
+void timer_callback(void) {
+  unix_time_ms++;
+  if (unix_time_ms > 1000) {
+    unix_time++;
+    unix_time_ms = 0;
+    settings.update_display = 1;
+  }
+}
 
 void setup() {
-  //init SPI driver for '595
-  Serial.begin(115200);
-  Serial.println();
-  SPI.begin();
-  //latch pin for '595
-  pinMode(LATCH_pin, OUTPUT);
-  //output enable (low) for '595
-  pinMode(OE_pin, OUTPUT);
-  //analogWrite(OE_pin, 10);
-  digitalWrite(OE_pin, LOW);
-  //HVPS enable
-  pinMode(HVEN_pin, OUTPUT);
-  digitalWrite(HVEN_pin, HIGH);
 
-  ticks.attach_ms(1000, tock);
+  hw_setup();
+  animation.attach_ms(250, boot_animation);
 
-  //dumy init
-  local_time.hours = 17;
-  local_time.minutes = 35;
-  local_time.seconds = 00;
-  boot_animation();
+  //setup file system
+  fs_setup();
+  //set a nice hostname
+  sprintf(settings.hostname, "naked_nixie_%06X", ESP.getChipId());
+
+  //setup wifi
+  wifi_setup();
+
+  //setup web services
+  web_setup();
+
+  //check to see if time server is online (check internet connection also)
+  settings.online = Ping.ping(settings.time_server) > 0 ? true : false;
+
+  //enable timer1 as time base with interrupt every 1ms
+  timer1_disable();
+  timer1_attachInterrupt(timer_callback);
+  timer1_enable(TIM_DIV16, TIM_EDGE, TIM_LOOP);
+  timer1_write(5000);
+  timer1_isr_init();
+
+  if (settings.online) {
+    unix_time = ntp_get_time();
+  }
+  //stop boot animation & start displaying
+  animation.detach();
+  update_displays();
 }
 
 void loop() {
-
-  if(settings.update_display) {
+  //check reboot flag
+  if (settings.reboot) {
+    Serial.println(F("[SYS] got restart request from the wire!"));
+    settings.reboot = false;
+    ESP.restart();
+  }
+  //check soft_ap flag to handle DNS requests
+  if (settings.soft_ap) {
+    webui_dns_requests();
+  }
+  //update display if necesary
+  if (settings.update_display) {
     settings.update_display = 0;
-    slot();
-    //Serial.printf("time is: %2d:%2d\n", local_time.hours, local_time.minutes);
+    update_displays();
+  }
+  //check update time flag or next update update
+  if ((settings.update_time || millis() > settings.next_ntp_update) && settings.online) {
+    Serial.println(F("[NTP] time for an update!"));
+    settings.update_time = 0;
+    unix_time = ntp_get_time();
   }
 }
 
-void tock() {
-  old_time = local_time;
-  local_time.seconds += 1;
-  if (local_time.seconds > 59) {
-    local_time.seconds = 0;
-    local_time.minutes += 1;
-    settings.update_display = 1;
-  }
-  if (local_time.minutes > 59) {
-    local_time.minutes = 0;
-    local_time.hours += 1;
-  }
-  if (local_time.hours > 23) {
-    local_time.hours = 0;
-  }
-  
-}
-
-void boot_animation() {
-  uint8_t data[4] = {0};
-  uint8_t digit;
-  for(digit = 0; digit < 10; digit++) {
-    data[0] = data[1] = data[2] = data[3] = digit;
-    update_displays(data);
-    delay(250);
-  }
-  data[0] = local_time.hours / 10;
-  data[1] = local_time.hours % 10;
-  data[2] = local_time.minutes / 10;
-  data[3] = local_time.minutes % 10;
-  update_displays(data);
-}
-
-void slot() {
-  
-  uint8_t old_values[4] = {0};
-  uint8_t new_values[4] = {0};
-
-  //break new/old time into individual digits
-  //old time
-  old_values[0] = (old_time.hours / 10);
-  old_values[1] = (old_time.hours % 10);
-  old_values[2] = (old_time.minutes / 10);
-  old_values[3] = (old_time.minutes % 10);
-  
-  //new time
-  new_values[0] = (local_time.hours / 10);
-  new_values[1] = (local_time.hours % 10);
-  new_values[2] = (local_time.minutes / 10);
-  new_values[3] = (local_time.minutes % 10);
-  
-  uint8_t frames[11][4] = {0};
-  uint8_t frame_idx = 0;
-  uint8_t digit;
-  uint8_t value;
-
-  for (digit = 0; digit < 4; digit++) {
-    if (new_values[digit] != old_values[digit] ) {
-      
-      value = new_values[digit];
-      for (frame_idx = 0; frame_idx < 11; frame_idx++) {
-        frames[frame_idx][digit]=value++;
-        if(value > 9) {
-          value = 0;
-        }
-      }
-    } else {
-      for (frame_idx = 0; frame_idx < 11; frame_idx++) {
-        frames[frame_idx][digit] = old_values[digit];
-      }
-    }
-  }
-
-  for (frame_idx = 0; frame_idx < 11; frame_idx++) {
-    //Serial.printf("Frame [%2d]: value [%2d:%2d:%2d:%2d]\n", frame_idx, frames[frame_idx][0], frames[frame_idx][1], frames[frame_idx][2], frames[frame_idx][3]);
-    update_displays(frames[frame_idx]);
-    delay(100);
-  }
-}
-
-void update_displays(uint8_t *data) {
-
-  //int16_t data[4] = {0};
-  //data[0] = 1 << (hours / 10);
-  //data[1] = 1 << (hours % 10);
-  //data[2] = 1 << (minutes / 10);
-  //data[3] = 1 << (minutes % 10);
-
-  //display 4
-  SPI.transfer((uint8_t)(((1 << data[3]) & 0xFF00) >> 8));
-  SPI.transfer((uint8_t)((1 << data[3]) & 0x00FF));
-  //display 3
-  SPI.transfer((uint8_t)(((1 << data[2]) & 0xFF00) >> 8));
-  SPI.transfer((uint8_t)((1 << data[2]) & 0x00FF));
-  //display 2
-  SPI.transfer((uint8_t)(((1 << data[1]) & 0xFF00) >> 8));
-  SPI.transfer((uint8_t)((1 << data[1]) & 0x00FF));
-  //display 1
-  SPI.transfer((uint8_t)(((1 << data[0]) & 0xFF00) >> 8));
-  SPI.transfer((uint8_t)((1 << data[0]) & 0x00FF));
-
-  //toggle latch pin
-  digitalWrite(LATCH_pin, LOW);
-  digitalWrite(LATCH_pin, HIGH);
-  digitalWrite(LATCH_pin, LOW);
-}
